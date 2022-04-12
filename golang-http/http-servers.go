4dcc4a9727965ff50819d6b561273cf1e08081cc
sekolahlinux <akbar@flip.id>
2022-04-12 23:03:12 +0700

    update

---
+++ golang-http/http-servers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+    fmt.Fprintf(w, "hello\n")
+}
+
+func healthz(w http.ResponseWriter, req *http.Request) {
+
+    fmt.Fprintf(w, "ok\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
+
+func main() {
+
+    http.HandleFunc("/", hello)
+    http.HandleFunc("/healthz", healthz)
+    http.HandleFunc("/headers", headers)
+
+    http.ListenAndServe(":8080", nil)
+}
